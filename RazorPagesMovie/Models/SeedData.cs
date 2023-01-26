febffc505f8188aa876d5ca984195f216bef2f0b
fineboy0407 <levilee.fine@gmail.com>
2023-01-27 04:24:41 +0900

    Seed the database

---
+++ RazorPagesMovie/Models/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Data;
+
+namespace RazorPagesMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(serviceProvider.GetRequiredService<DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                // Look for any movies
+                if(context.Movie.Any())
+                {
+                    return; // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "When Harry meet Sally",
+                        ReleaseDate = DateTime.Parse("1990-2-3"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ghostbusters",
+                        ReleaseDate = DateTime.Parse("1999-11-3"),
+                        Genre = "Comedy",
+                        Price = 8.88M,
+                    },
+
+                    new Movie
+                    {
+                        Title = "Rio Bavo",
+                        ReleaseDate = DateTime.Parse("2019-2-1"),
+                        Genre = "Western",
+                        Price = 2.5M,
+                    }
+
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
