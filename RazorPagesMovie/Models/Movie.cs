30070ec7537d1568cded282e408f05ce1c278141
fineboy0407 <levilee.fine@gmail.com>
2023-01-27 11:33:36 +0900

    Update attributes of the fields in Model

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -6,9 +8,14 @@ namespace RazorPagesMovie.Models
     {
         public int Id { get; set; }
         public string? Title { get; set; }
+
+        [Display(Name="Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public string? Genre { get; set; }
+
+        [Column(TypeName ="decimal(18,2)")]
         public decimal? Price { get; set; }
     }
 }
