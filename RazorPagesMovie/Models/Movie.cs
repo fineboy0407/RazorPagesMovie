9b141efe2934a6655c8a50bbaf7c7d0fc6328371
fineboy0407 <levilee.fine@gmail.com>
2023-01-28 17:51:56 +0900

    Add new Rating Field

--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -7,15 +7,17 @@ namespace RazorPagesMovie.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
         [Display(Name="Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        public string? Genre { get; set; }
+        public string Genre { get; set; } = string.Empty;
 
         [Column(TypeName ="decimal(18,2)")]
         public decimal? Price { get; set; }
+
+        public string Rating { get; set; } = string.Empty;
     }
 }
